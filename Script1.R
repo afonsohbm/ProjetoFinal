34433c8dd07302626f0cfb4d00cfd79726eff1d4
Afonso Barros Machado <afonso.machado@spinengenharia.local>
2020-05-19 21:08:47 -0300

    Primeiros artigos e início da exploração do código

---
+++ Script1.R
@@ -0,0 +1,28 @@
+library(electionsBR)
+library(dplyr)
+
+
+candidate <- electionsBR::candidate_fed(year = 2018, uf = "DF")
+View(candidate)
+
+candidateFiltered <- filter(candidate, SIGLA_PARTIDO == "NOVO")
+View(candidateFiltered)
+
+municipios <- electionsBR::details_mun_zone_fed(year = 2018, uf = "DF")
+View(municipios)
+
+party_mun_zone_fed <- electionsBR::party_mun_zone_fed(year = 2018, uf = "DF")
+View(party_mun_zone_fed)
+
+party_mun_zone_fed_FILTERED <- filter(party_mun_zone_fed, SIGLA_PARTIDO == "NOVO")
+View(party_mun_zone_fed_FILTERED)
+
+filter(party_mun_zone_fed_FILTERED, DESCRICAO_CARGO == "Presidente")
+
+finance <- electionsBR::personal_finances_fed(year = 2018, uf = "DF")
+View(finance)
+
+affiliation <- electionsBR::voter_affiliation(party  = "NOVO", uf = "DF")
+
+senador <- party_mun_zone_fed[party_mun_zone_fed$DESCRICAO_CARGO == "Senador",]
+presidente <- party_mun_zone_fed[party_mun_zone_fed$DESCRICAO_CARGO == "Presidente",]
